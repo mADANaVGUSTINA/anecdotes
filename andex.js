bf3a6d7122e6e2e441cef8e7e4f5abf424ee33b5
mADANaVGUSTINA <daniloex40@gmail.com>
2023-03-12 15:35:14 +0100

    adding likes and dislikes

--- andex.js
+++ andex.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 const port = 3000;
 let server = http.createServer(function(request, response){
     if(request.url === "/api/jokes" && request.method === 'GET'){
@@ -16,7 +17,27 @@ let server = http.createServer(function(request, response){
         request.on('end', function(){
             addJoke(data);
         })
-        response.end
+        response.end()
+    }
+    else if(request.url.startsWith('/api/likes')){
+        let reqUrl = request.url
+        if(LikeOrDislike(reqUrl, true)){
+            response.writeHead(200);
+            response.end();
+        }else{
+            response.writeHead(400);
+            response.end();
+        }
+    }
+    else if(request.url.startsWith('/api/dislikes')){
+        let reqUrl = request.url
+        if(LikeOrDislike(reqUrl, false)){
+            response.writeHead(200);
+            response.end();
+        }else{
+            response.writeHead(400);
+            response.end();
+        }
     }
 });
 function getAllJokes(request, response){
@@ -36,6 +57,27 @@ function addJoke(joke){
     let pthD = path.join(__dirname, "data");
     let pthf = path.join(pthD, `${fs.readdirSync(pthD).length}.json`)
     fs.writeFileSync(pthf , JSON.stringify(jokedata));
+}
+function LikeOrDislike(reqUrl, chek){
+    const params = url.parse(reqUrl, true).query;
+        if(isNaN(params.id)){
+            return false
+        }
+        let pthD = path.join(__dirname, "data");
+        let jknumb = fs.readdirSync(pthD).length;
+        if(params.id < 0 || params.id >= jknumb){
+            return false
+        }
+    let pthF = path.join(pthD, `${params.id}.json`);
+    let joker = JSON.parse(fs.readFileSync(pthF, "utf-8"));
+    if(chek == true){
+        joker.likes++;
+    }
+    else{
+        joker.dislikes++;
+    }
+    fs.writeFileSync(pthF, JSON.stringify(joker));
+    return true
 }
 
 server.listen(port);
